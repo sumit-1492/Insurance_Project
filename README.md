21fe4c83e5305f662f292b821f1583b8ef50ffbc
sumit-1492 <sumitlala99@gmail.com>
2023-03-02 22:58:13 +0530

    folder structure and data dump completed

--- README.md
+++ README.md
@@ -5,9 +5,20 @@
 ### to create enviornment for any DS project
 ```
 conda create --prefix ./env python=3.7 -y
-
 ```
 ### to activate environment
 ```
 conda activate ./env
 ```
+### In this project everything done on the cloud
+
+### to create any file using git bash
+```
+touch filename
+```
+# data_dump.py file created in which we will dump our data in mongodb database
+# data inserted to database
+
+### creating script to get all the required files in our project
+
+### template.py created it will be used as generic file for any project to develope
---
+++ data_dump.py
@@ -0,0 +1,21 @@
+import pymongo
+import pandas as pd
+import json
+
+
+client = pymongo.MongoClient("mongodb+srv://se2zT5RUfhSvQmau:se2zT5RUfhSvQmau@cluster0.8peshl5.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+DATA_FILE_PATH = (r"E:\Insurance_Project\insurance.csv")
+DATABASE_NAME = "Insurance"
+COLLECTION_NAME =  "Insurance_Project"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns:{df.shape}")
+
+    df.reset_index(drop = True,inplace = True)
+    json_record = list(json.loads(df.T.to_json()).values())
+
+    print(json_record[0])
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
